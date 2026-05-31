8b59a0d0316e1b516ae7144af74cdee4242654b0
deniskrastev9-cpu <deniskrastev9@gmail.com>
2026-05-31 00:14:27 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import MissedCallTextBack from "./pages/MissedCallTextBack";
 import CustomerFollowUpAutomation from "./pages/CustomerFollowUpAutomation";
 import LeadCaptureAutomation from "./pages/LeadCaptureAutomation";
 import CRMAutomationSystem from "./pages/CRMAutomationSystem";
+import AiReceptionist from "./pages/AiReceptionist";
 
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import Terms from "./pages/Terms";
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/services/customer-follow-up" element={<CustomerFollowUpAutomation />} />
         <Route path="/services/lead-capture" element={<LeadCaptureAutomation />} />
         <Route path="/services/crm-automation" element={<CRMAutomationSystem />} />
+        <Route path="/services/ai-receptionist" element={<AiReceptionist />} />
 
         <Route path="/privacy" element={<PrivacyPolicy />} />
         <Route path="/terms" element={<Terms />} />
